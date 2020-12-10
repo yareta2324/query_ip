49cebfa0dec71c21597d7a0d43e33b7dede11029
yareta <nash_xiang@163.com>
2020-12-11 01:26:31 +0800

    update code

--- app.py
+++ app.py
@@ -5,28 +5,25 @@ from common.base_ import query
 
 app = Flask(__name__)
 limiter = Limiter(
-    app, key_func=get_remote_address, default_limits=["48000 per day", "2000 per hour"]
+    app,
+    key_func=get_remote_address,
+    default_limits=["48000 per day", "2000 per hour"]
 )
 
 
 @app.route('/', methods=['GET'])
 @limiter.limit("1/second", override_defaults=False)
 def index():
-    """
-    browser and the CURL route
-    :return:
-    """
-    curl, remote = request.headers, request.headers.get('X-Real-IP')
-    return query(args=remote, header=curl)
+    """browser and the CURL route"""
+    rel_ip = request.headers.get('X-Real-IP')
+    header = request.headers
+    return query(args=rel_ip, header=header)
 
 
 @app.route('/v1/', methods=['GET'])
 @limiter.limit("1/second", override_defaults=False)
 def main_():
-    """
-    Responding to form queries
-    :return:
-    """
+    """Responding to form queries"""
     request_ = request.args.get('address')
     if request_:
         return query(args=request_, header=None)
